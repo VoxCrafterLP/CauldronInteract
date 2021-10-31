f46b7fa68d88c79b43624ff674f3e0de971d26e6
VoxCrafterLP <voxcrafterlp@gmail.com>
2021-10-31 16:44:15 +0100

    Fix BlockDispenseListener not being registered Add that legacy material support would be initialized at startup

--- src/main/java/com/voxcrafterlp/cauldroninteract/CauldronInteract.java
+++ src/main/java/com/voxcrafterlp/cauldroninteract/CauldronInteract.java
@@ -4,6 +4,7 @@ import com.voxcrafterlp.cauldroninteract.listener.BlockDispenseListener;
 import lombok.Getter;
 import org.bstats.bukkit.Metrics;
 import org.bukkit.Bukkit;
+import org.bukkit.craftbukkit.v1_17_R1.legacy.CraftLegacy;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -28,11 +29,12 @@ public class CauldronInteract extends JavaPlugin {
 
         this.registerListener();
         this.loadMetrics();
+        this.initializeLegacyMaterialSupport();
     }
 
     private void registerListener() {
         final PluginManager pluginManager = Bukkit.getPluginManager();
-        //pluginManager.registerEvents(new BlockDispenseListener(), this);
+        pluginManager.registerEvents(new BlockDispenseListener(), this);
     }
 
     /**
@@ -41,6 +43,15 @@ public class CauldronInteract extends JavaPlugin {
     private void loadMetrics() {
         final int pluginId = 12031;
         this.metrics = new Metrics(this, pluginId);
+    }
+
+    /**
+     * Initializes legacy material support
+     */
+    private void initializeLegacyMaterialSupport() {
+        Bukkit.getConsoleSender().sendMessage("§7[§bCauldronInteract§7] §7Initializing legacy material support. This can take a while!");
+        CraftLegacy.init();
+        Bukkit.getConsoleSender().sendMessage("§7[§bCauldronInteract§7] §aDone!");
     }
 
 }
